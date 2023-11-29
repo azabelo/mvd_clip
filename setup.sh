12af3ae1476b34a46d9754a734427b546ce7e205
azabelo <azabelo21old@gmail.com>
2023-11-28 18:57:38 -0800

    copied original mvd files

--- setup.sh
+++ setup.sh
@@ -51,110 +51,106 @@ if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "downloading HMDB-51 dataset"
               ######## DOWNLOAD DATASET #########
 
-          # Specify the URL of the file to download
-          curl https://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/hmdb51_org.rar --output hmdb51_dataset.rar
 
-          # Install unrar if not already installed
-          if ! command -v unrar &> /dev/null; then
-              echo "Installing unrar..."
-              sudo apt-get install unrar
-          fi
+# Specify the URL of the file to download
+curl https://serre-lab.clps.brown.edu/wp-content/uploads/2013/10/hmdb51_org.rar --output hmdb51_dataset.rar
 
-          #unzip big file
-          mkdir hmdb51_unrared
-          mv hmdb51_dataset.rar hmdb51_unrared/hmdb51_dataset.rar
-          cd hmdb51_unrared
-          unrar x hmdb51_dataset.rar
+# Install unrar if not already installed
+if ! command -v unrar &> /dev/null; then
+    echo "Installing unrar..."
+    sudo apt-get install unrar
+fi
+
+#unzip big file
+mkdir hmdb51_unrared
+mv hmdb51_dataset.rar hmdb51_unrared/hmdb51_dataset.rar
+cd hmdb51_unrared
+unrar x hmdb51_dataset.rar
 
 
-          ######### UNRAR #########
+######### UNRAR #########
 
-          # Get the current directory
-          directory=$(pwd)
-          # Check if the directory exists
-          if [ ! -d "$directory" ]; then
-            echo "Error: Directory '$directory' not found."
-            exit 1
-          fi
+# Get the current directory
+directory=$(pwd)
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+  echo "Error: Directory '$directory' not found."
+  exit 1
+fi
 
-          # Change to the target directory
-          cd "$directory"
+# Change to the target directory
+cd "$directory"
 
-          # Extract .rar files
-          rar_files=$(ls -p | grep -v / | grep -e "\.rar$")
-          for rar_file in $rar_files; do
-              unrar x "$rar_file"
-              rm "$rar_file"
-          done
+# Extract .rar files
+rar_files=$(ls -p | grep -v / | grep -e "\.rar$")
+for rar_file in $rar_files; do
+    unrar x "$rar_file"
+    rm "$rar_file"
+done
 
-          echo "Extraction complete. .rar files in $directory have been unrar'd."
-          cd ..
+echo "Extraction complete. .rar files in $directory have been unrar'd."
+cd ..
 
-          ########## CONVERT TO MP4 ##########
+########## CONVERT TO MP4 ##########
 
-          # Install ffmpeg if not already installed
-          if ! command -v ffmpeg &> /dev/null; then
-              echo "Installing ffmpeg..."
-              # Use the appropriate package manager based on your system (apt-get for Ubuntu/Debian)
-              # Replace 'apt-get' with 'brew' for macOS using Homebrew or 'yum' for some Linux distributions
-              sudo apt-get install ffmpeg
-          fi
+# Install ffmpeg if not already installed
+if ! command -v ffmpeg &> /dev/null; then
+    echo "Installing ffmpeg..."
+    # Use the appropriate package manager based on your system (apt-get for Ubuntu/Debian)
+    # Replace 'apt-get' with 'brew' for macOS using Homebrew or 'yum' for some Linux distributions
+    sudo apt-get install ffmpeg
+fi
 
-#          # Function to convert .avi to .mp4 using ffmpeg
-#          convert_avi_to_mp4() {
-#            local input_file="$1"
-#            local output_file="$2"
-#
-#            ffmpeg -i "$input_file" "$output_file"
-#          }
+# Function to convert .avi to .mp4 using ffmpeg
+convert_avi_to_mp4() {
+  local input_file="$1"
+  local output_file="$2"
 
-          # Function to convert .avi to .mp4 using ffmpeg
-          convert_avi_to_mp4() {
-            local input_file="$1"
-            local output_file="$2"
+  ffmpeg -i "$input_file" "$output_file"
+}
 
-            ffmpeg -i "$input_file" -c:v copy -bsf:v mpeg4_unpack_bframes "$output_file"
-          }
+# Main script
+input_directory="hmdb51_unrared"  # Replace this with the path to your input directory
+output_directory="hmdb51_mp4"  # Replace this with the path to the output directory
 
-          # Main script
-          input_directory="hmdb51_unrared"  # Replace this with the path to your input directory
-          output_directory="hmdb51_mp4"  # Replace this with the path to the output directory
+# Check if input directory exists
+if [ ! -d "$input_directory" ]; then
+  echo "Error: Input directory '$input_directory' not found."
+  exit 1
+fi
 
-          # Check if input directory exists
-          if [ ! -d "$input_directory" ]; then
-            echo "Error: Input directory '$input_directory' not found."
-            exit 1
-          fi
+# Create output directory if it doesn't exist
+mkdir -p "$output_directory"
 
-          # Create output directory if it doesn't exist
-          mkdir -p "$output_directory"
+# Find all subdirectories in the input directory
+subdirectories=$(find "$input_directory" -type d -mindepth 1)
 
-          # Find all subdirectories in the input directory
-          subdirectories=$(find "$input_directory" -type d -mindepth 1)
+# Loop through each subdirectory and convert .avi files to .mp4
+for subdirectory in $subdirectories; do
+  # Create corresponding subdirectory in the output directory
+  relative_path="${subdirectory#"$input_directory"/}"
+  output_subdirectory="$output_directory/$relative_path"
+  mkdir -p "$output_subdirectory"
 
-          # Loop through each subdirectory and convert .avi files to .mp4
-          for subdirectory in $subdirectories; do
-            # Create corresponding subdirectory in the output directory
-            relative_path="${subdirectory#"$input_directory"/}"
-            output_subdirectory="$output_directory/$relative_path"
-            mkdir -p "$output_subdirectory"
+  # Find all .avi files in the current subdirectory
+  avi_files=$(find "$subdirectory" -type f -name "*.avi")
 
-            # Find all .avi files in the current subdirectory
-            avi_files=$(find "$subdirectory" -type f -name "*.avi")
+  # Convert .avi to .mp4 for each file found
+  for avi_file in $avi_files; do
+    # Get the relative path of the input file within the subdirectory
+    relative_file_path="${avi_file#"$subdirectory"/}"
+    # Construct the corresponding output path within the output subdirectory
+    output_path="$output_subdirectory/${relative_file_path%.avi}.mp4"
 
-            # Convert .avi to .mp4 for each file found
-            for avi_file in $avi_files; do
-              # Get the relative path of the input file within the subdirectory
-              relative_file_path="${avi_file#"$subdirectory"/}"
-              # Construct the corresponding output path within the output subdirectory
-              output_path="$output_subdirectory/${relative_file_path%.avi}.mp4"
+    # Convert the .avi file to .mp4 using ffmpeg
+    convert_avi_to_mp4 "$avi_file" "$output_path"
+  done
+done
 
-              # Convert the .avi file to .mp4 using ffmpeg
-              convert_avi_to_mp4 "$avi_file" "$output_path"
-            done
-          done
+echo "Conversion complete. .avi files in $input_directory and its subdirectories have been converted to .mp4 files in $output_directory."
 
-          echo "Conversion complete. .avi files in $input_directory and its subdirectories have been converted to .mp4 files in $output_directory."
+
+
 
           ######## MAKE CSV FILE ############
 #
