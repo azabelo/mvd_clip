d526166ded6b27bfcb09c01aa7ea88d351d2c89b
azabelo <azabelo21old@gmail.com>
2023-11-28 21:21:47 -0800

    copied original mvd files

--- create_pretrain_csv.sh
+++ create_pretrain_csv.sh
@@ -22,7 +22,13 @@ output_csv="official_pretrain.csv"
 count=0
 while read -r video_path _; do
   # Extract frame count using ffprobe (assuming ffprobe is available)
-  frame_count=$(ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "$video_path" || echo "N/A")
+  frame_count=$(ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "$video_path")
+
+  # Check if frame_count is empty (indicating an error in ffprobe)
+  if [ -z "$frame_count" ]; then
+    echo "Error: 'N/A' encountered in $video_path. Skipping..."
+    continue
+  fi
 
   # Append the row to the output CSV file
   echo "$video_path $frame_count" >> "$output_csv"
