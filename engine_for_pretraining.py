26e6a040c1aadd8b064631e31e2cb75fbbffc012
azabelo <azabelo21old@gmail.com>
2023-11-29 04:27:57 -0800

    pretraining accuracy

--- engine_for_pretraining.py
+++ engine_for_pretraining.py
@@ -227,7 +227,7 @@ def pretraining_accuracy(model, args):
     # Instantiate the model
     linear_model = LinearClassifier()
     linear_criterion = nn.CrossEntropyLoss()
-    linear_optimizer = optim.SGD(linear_model.parameters(), lr=0.05)
+    linear_optimizer = optim.SGD(linear_model.parameters(), lr=0.005)
 
 
     class TwoLayerClassifier(nn.Module):
@@ -247,7 +247,7 @@ def pretraining_accuracy(model, args):
 
     # Define loss function and optimizer
     two_layer_criterion = nn.CrossEntropyLoss()
-    two_layer_optimizer = optim.SGD(two_layer_model.parameters(), lr=0.05)
+    two_layer_optimizer = optim.SGD(two_layer_model.parameters(), lr=0.005)
 
     # move everything to the GPU
     linear_model = linear_model.to(args_copy.device)
