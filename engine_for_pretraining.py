1a0181c0c1cc8b5d7cdc22589a55c8f660638da7
azabelo <azabelo21old@gmail.com>
2023-12-19 16:03:05 -0500

    videomaev2 debugging

--- engine_for_pretraining.py
+++ engine_for_pretraining.py
@@ -64,14 +64,16 @@ def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable, optimiz
                 empty_mask = empty_mask.to(args.device)
                 ones_features, ones_vid_feats = model(torch.ones((1, 3, 16, 224, 224)).cuda(), empty_mask)
                 student_feats = model.module.forward_encoder(torch.ones((1, 3, 16, 224, 224)).cuda(), empty_mask)
-                # print("ones image teacher features (prior to training): ", ones_features[:, 0, :25])
-                # print("ones video teacher features (prior to training): ", ones_vid_feats[:, 0, :25])
-                # print("ones student features (prior to training): ", student_feats[:, 0, :25])
+                print("student image decoded features (prior to training): ", ones_features[:, 0, :25])
+                print("student video decoded features (prior to training): ", ones_vid_feats[:, 0, :25])
+                # print("student features (prior to training): ", student_feats[:, 0, :25])
+                # if you use cls you should look at the second index to compare with video teacher
+                print("student features (prior to training): ", student_feats[:, 1, :25])
 
                 # test that the output of the video teacher doesn't change by passing in a ones vector
                 # (found that it doesn't change)
                 ones_video_features = video_teacher_model(torch.ones((1, 3, 16, 224, 224)).cuda())
-                print("video teacher feats (after knn): ", ones_video_features[:, 0, :25])
+                print("video teacher feats (epoch start): ", ones_video_features[:, 0, :25])
 
 
     # END MY CHANGES
@@ -215,7 +217,7 @@ def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable, optimiz
 
 
 def pretraining_accuracy(model, video_teacher_model, args):
-    test_teacher = True
+    test_teacher = False
     if test_teacher:
         model = video_teacher_model
 
