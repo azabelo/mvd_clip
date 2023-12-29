017bb4a68f70e9ca92c7a7b1ed9acdbb4230e1d7
azabelo <azabelo21old@gmail.com>
2023-12-28 20:57:28 -0500

    naive zero shot

--- engine_for_pretraining.py
+++ engine_for_pretraining.py
@@ -420,10 +420,8 @@ def pretraining_accuracy(model, video_teacher_model, args):
             clip_model, preprocess = clip.load("ViT-B/16", device=args.device)
             # multiply the features by the model.visual.proj matrix (not to be done when model is the teacher)
             clip_space_features = torch.matmul(cls_token, clip_model.visual.proj.float())
+            clip_space_features /= clip_space_features.norm(dim=-1, keepdim=True)
             # for each of the features, find the cosine similarity with each of the text features
-            print("text encodings 1831:", text_encodings[1831])
-            print("text encodings 1832:", text_encodings[1832])
-            print("text encodings 1833:", text_encodings[1833])
             tensor1 = clip_space_features.unsqueeze(1)
             tensor2 = text_encodings.unsqueeze(0)
             cosine_sim = torch.nn.functional.cosine_similarity(tensor1, tensor2, dim=2)
