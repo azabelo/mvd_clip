927953f7918f4ec319347623169c530078a2877a
azabelo <azabelo21old@gmail.com>
2023-12-30 21:02:53 -0500

    naive zero shot

--- create_cos_heatmap.py
+++ create_cos_heatmap.py
@@ -9,6 +9,7 @@ file_path = "vid_encodings.pth"
 #file_path = "text_encodings.pth"
 
 video_encodings = torch.load(file_path)
+video_encodings = video_encodings[:video_encodings.shape[0], ...]
 
 vid_cosine_similarities = torch.nn.functional.cosine_similarity(video_encodings.unsqueeze(0),
                                                                 video_encodings.unsqueeze(1), dim=-1)
