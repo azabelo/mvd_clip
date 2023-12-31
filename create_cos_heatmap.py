0d1d12900149d41dd9622b399f6a011fd0eb1f76
azabelo <azabelo21old@gmail.com>
2023-12-30 21:00:23 -0500

    naive zero shot

---
+++ create_cos_heatmap.py
@@ -0,0 +1,28 @@
+import torch
+import clip
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path = "vid_encodings.pth"
+#file_path = "img_encodings.pth"
+#file_path = "text_encodings.pth"
+
+video_encodings = torch.load(file_path)
+
+vid_cosine_similarities = torch.nn.functional.cosine_similarity(video_encodings.unsqueeze(0),
+                                                                video_encodings.unsqueeze(1), dim=-1)
+print(vid_cosine_similarities.shape)
+# Set the figure size and create the heatmap
+fig, ax = plt.subplots(figsize=(3570 / 100, 3570 / 100))
+sns.heatmap(vid_cosine_similarities.cpu().numpy(), cmap="viridis", xticklabels=False, yticklabels=False, cbar=True,
+            ax=ax)
+
+# Set the DPI to control the image size
+dpi = 100
+fig.set_dpi(dpi)
+# Save the heatmap image with the desired resolution
+heatmap_path = "video_cosine_similarity_heatmap.png"
+plt.savefig(heatmap_path, dpi=dpi)
+plt.close()
+print(f"Heatmap saved to {heatmap_path}")
