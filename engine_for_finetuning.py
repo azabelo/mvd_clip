273061b8a7badeeebad6e019aedce295f74dfaba
azabelo <azabelo21old@gmail.com>
2024-01-17 02:08:35 -0800

    clip style loss

--- engine_for_finetuning.py
+++ engine_for_finetuning.py
@@ -306,18 +306,12 @@ def compute_video(lst):
 
 
 
-def align_class_batch(model, samples, target, criterion):
+def align_class_batch(model, samples, text, criterion):
 
-    outputs = model(samples)
-    loss = criterion(outputs, target)
-    # MY CHANGES
-    # print("targets")
-    # print(target)
-    # print("outputs")
-    # print(outputs)
-    # END MY CHANGES
+    loss = model(samples, text)
 
-    return loss, outputs
+    # outputs
+    return loss
 
 def align_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
@@ -354,89 +348,117 @@ def align_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         samples = samples.to(device, non_blocking=True)
         targets = targets.to(device, non_blocking=True)
 
-        if mixup_fn is not None:
-            samples, targets = mixup_fn(samples, targets)
+        action_embeddings = torch.load("action_encodings.pth")
+        class_names_str = "brush_hair clap draw_sword fall_floor handstand kick pick push run shoot_gun smoke sword turn cartwheel climb dribble fencing hit kick_ball pour pushup shake_hands sit somersault sword_exercise walk catch climb_stairs drink flic_flac hug kiss pullup ride_bike shoot_ball situp stand talk wave chew dive eat golf jump laugh punch ride_horse shoot_bow smile swing_baseball throw"
+        all_action_names = ['brushing hair', 'doing a cartwheel', 'catching', 'chewing', 'clapping', 'climbing',
+                            'climbing stairs', 'diving', 'drawing a sword', 'dribbling', 'drinking', 'eating',
+                            'falling to the floor', 'fencing', 'doing flic flac', 'golfing', 'doing a handstand',
+                            'hitting',
+                            'hugging', 'jumping', 'kicking', 'kicking a ball', 'kissing', 'laughing', 'picking',
+                            'pouring',
+                            'doing pullups', 'punching', 'pushing', 'doing pushups', 'riding a bike',
+                            'riding a horse',
+                            'running', 'shaking hands', 'shooting a ball', 'shooting a bow', 'shooting a gun',
+                            'sitting',
+                            'doing situps', 'smiling', 'smoking', 'doing a somersault', 'standing',
+                            'swinging a baseball bat',
+                            'using a sword', 'doing sword exercises', 'talking', 'throwing', 'turning', 'walking',
+                            'waving']
+        all_class_names = class_names_str.split()
+        templates = [
+            'a photo of a person {}.',
+            'a video of a person {}.',
+            'a example of a person {}.',
+            'a demonstration of a person {}.']
 
-        if loss_scaler is None:
-            samples = samples.half()
-            loss, output = align_class_batch(
-                model, samples, targets, criterion)
-        else:
-            with torch.cuda.amp.autocast():
-                loss, output = align_class_batch(
-                    model, samples, targets, criterion)
+        # repeat the training loop for each template
+        for template in templates:
+            text = [template.format(all_action_names[targets[i]]) for i in range(len(targets))]
 
-        loss_value = loss.item()
+            if mixup_fn is not None:
+                samples, targets = mixup_fn(samples, targets)
 
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            sys.exit(1)
+            if loss_scaler is None:
+                samples = samples.half()
+                loss = align_class_batch(
+                    model, samples, text, criterion)
+            else:
+                with torch.cuda.amp.autocast():
+                    loss = align_class_batch(
+                        model, samples, text, criterion)
 
-        if loss_scaler is None:
-            loss /= update_freq
-            model.backward(loss)
-            model.step()
+            loss_value = loss.item()
 
-            if (data_iter_step + 1) % update_freq == 0:
-                # model.zero_grad()
-                # Deepspeed will call step() & model.zero_grad() automatic
-                if model_ema is not None:
-                    model_ema.update(model)
-            grad_norm = None
-            loss_scale_value = get_loss_scale_for_deepspeed(model)
-        else:
-            # this attribute is added by timm on one optimizer (adahessian)
-            is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
-            loss /= update_freq
-            grad_norm = loss_scaler(loss, optimizer, clip_grad=max_norm,
-                                    parameters=model.parameters(), create_graph=is_second_order,
-                                    update_grad=(data_iter_step + 1) % update_freq == 0)
-            if (data_iter_step + 1) % update_freq == 0:
-                optimizer.zero_grad()
-                if model_ema is not None:
-                    model_ema.update(model)
-            loss_scale_value = loss_scaler.state_dict()["scale"]
+            if not math.isfinite(loss_value):
+                print("Loss is {}, stopping training".format(loss_value))
+                sys.exit(1)
 
-        torch.cuda.synchronize()
+            if loss_scaler is None:
+                loss /= update_freq
+                model.backward(loss)
+                model.step()
 
-        if mixup_fn is None:
-            class_acc = (output.max(-1)[-1] == targets).float().mean()
-        else:
-            class_acc = None
-        metric_logger.update(loss=loss_value)
-        metric_logger.update(class_acc=class_acc)
-        metric_logger.update(loss_scale=loss_scale_value)
-        min_lr = 10.
-        max_lr = 0.
-        for group in optimizer.param_groups:
-            min_lr = min(min_lr, group["lr"])
-            max_lr = max(max_lr, group["lr"])
+                if (data_iter_step + 1) % update_freq == 0:
+                    # model.zero_grad()
+                    # Deepspeed will call step() & model.zero_grad() automatic
+                    if model_ema is not None:
+                        model_ema.update(model)
+                grad_norm = None
+                loss_scale_value = get_loss_scale_for_deepspeed(model)
+            else:
+                # this attribute is added by timm on one optimizer (adahessian)
+                is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
+                loss /= update_freq
+                grad_norm = loss_scaler(loss, optimizer, clip_grad=max_norm,
+                                        parameters=model.parameters(), create_graph=is_second_order,
+                                        update_grad=(data_iter_step + 1) % update_freq == 0)
+                if (data_iter_step + 1) % update_freq == 0:
+                    optimizer.zero_grad()
+                    if model_ema is not None:
+                        model_ema.update(model)
+                loss_scale_value = loss_scaler.state_dict()["scale"]
 
-        metric_logger.update(lr=max_lr)
-        metric_logger.update(min_lr=min_lr)
-        weight_decay_value = None
-        for group in optimizer.param_groups:
-            if group["weight_decay"] > 0:
-                weight_decay_value = group["weight_decay"]
-        metric_logger.update(weight_decay=weight_decay_value)
-        metric_logger.update(grad_norm=grad_norm)
+            torch.cuda.synchronize()
 
-        if log_writer is not None:
-            log_writer.update(loss=loss_value, head="loss")
-            log_writer.update(class_acc=class_acc, head="loss")
-            log_writer.update(loss_scale=loss_scale_value, head="opt")
-            log_writer.update(lr=max_lr, head="opt")
-            log_writer.update(min_lr=min_lr, head="opt")
-            log_writer.update(weight_decay=weight_decay_value, head="opt")
-            log_writer.update(grad_norm=grad_norm, head="opt")
+            class_acc = 0
+            # if mixup_fn is None:
+            #     class_acc = (output.max(-1)[-1] == targets).float().mean()
+            # else:
+            #     class_acc = None
+            # metric_logger.update(loss=loss_value)
+            # metric_logger.update(class_acc=class_acc)
+            # metric_logger.update(loss_scale=loss_scale_value)
+            min_lr = 10.
+            max_lr = 0.
+            for group in optimizer.param_groups:
+                min_lr = min(min_lr, group["lr"])
+                max_lr = max(max_lr, group["lr"])
 
-            log_writer.set_step()
+            metric_logger.update(lr=max_lr)
+            metric_logger.update(min_lr=min_lr)
+            weight_decay_value = None
+            for group in optimizer.param_groups:
+                if group["weight_decay"] > 0:
+                    weight_decay_value = group["weight_decay"]
+            metric_logger.update(weight_decay=weight_decay_value)
+            metric_logger.update(grad_norm=grad_norm)
 
-        # MY CHANGES
-        wandb.log({"epoch": epoch, "batch": step, "train_loss": loss_value, "max_lr": max_lr, "min_lr": min_lr,
-                   "weight_decay": weight_decay_value, "grad_norm": grad_norm, "loss_scale": loss_scale_value,
-                   "class_acc": class_acc})
-        # END MY CHANGES
+            if log_writer is not None:
+                log_writer.update(loss=loss_value, head="loss")
+                log_writer.update(class_acc=class_acc, head="loss")
+                log_writer.update(loss_scale=loss_scale_value, head="opt")
+                log_writer.update(lr=max_lr, head="opt")
+                log_writer.update(min_lr=min_lr, head="opt")
+                log_writer.update(weight_decay=weight_decay_value, head="opt")
+                log_writer.update(grad_norm=grad_norm, head="opt")
+
+                log_writer.set_step()
+
+            # MY CHANGES
+            wandb.log({"epoch": epoch, "batch": step, "train_loss": loss_value, "max_lr": max_lr, "min_lr": min_lr,
+                       "weight_decay": weight_decay_value, "grad_norm": grad_norm, "loss_scale": loss_scale_value,
+                       "class_acc": class_acc})
+            # END MY CHANGES
 
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
