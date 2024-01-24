59c9b9718f66bce2ef86d88cfa0dfe5901ea6a17
azabelo <azabelo21old@gmail.com>
2024-01-24 00:27:39 -0800

    clip style loss

--- zero_shot.py
+++ zero_shot.py
@@ -368,11 +368,9 @@ def precompute_train_video(model, data_loader):
             embedding = torch.mean(output, dim=1, keepdim=False)
             #embedding = output[:, 0, :] #.view(output.size(0), -1)
             # normalize each vector
-            # mean = torch.mean(embedding, dim=1, keepdim=True)
-            # std = torch.std(embedding, dim=1, keepdim=True)
-            #embedding = (embedding - mean) / std
-
-            embedding = embedding / torch.norm(embedding, dim=1, keepdim=True)
+            mean = torch.mean(embedding, dim=1, keepdim=True)
+            std = torch.std(embedding, dim=1, keepdim=True)
+            embedding = (embedding - mean) / std
             video_embeddings.append(embedding.cpu().numpy())
 
         video_embeddings = torch.tensor(np.concatenate(video_embeddings, axis=0), dtype=torch.float32)
@@ -409,9 +407,9 @@ def precompute_test_video(model, data_loader):
             embedding = torch.mean(output, dim=1, keepdim=False)
             #embedding = output[:, 0, :] #.view(output.size(0), -1)
             # normalize each vector
-            # mean = torch.mean(embedding, dim=1, keepdim=True)
-            # std = torch.std(embedding, dim=1, keepdim=True)
-            # embedding = (embedding - mean) / std
+            mean = torch.mean(embedding, dim=1, keepdim=True)
+            std = torch.std(embedding, dim=1, keepdim=True)
+            embedding = (embedding - mean) / std
 
             embedding = embedding / torch.norm(embedding, dim=1, keepdim=True)
             video_embeddings.append(embedding.cpu().numpy())
