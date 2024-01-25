97ef8f75bf81a66c4b63280ba606879d928d7a89
azabelo <azabelo21old@gmail.com>
2024-01-24 23:15:39 -0800

    clip style loss

--- engine_for_finetuning.py
+++ engine_for_finetuning.py
@@ -560,7 +560,8 @@ def efficient_align_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module
         print(linear_logits)
         print(targets.shape)
         print(targets)
-        linear_loss = linear_criterion(linear_logits, targets)
+
+        linear_loss = linear_criterion(linear_logits.cuda(), targets.cuda())
         print(linear_loss)
 
 
