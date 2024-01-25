5264e5318b7e58a9a8c8ff03bf643e5d5ecc66ce
azabelo <azabelo21old@gmail.com>
2024-01-24 23:46:02 -0800

    clip style loss

--- engine_for_finetuning.py
+++ engine_for_finetuning.py
@@ -566,6 +566,7 @@ def efficient_align_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module
         # print the grad of the linear layer
         print(linear_model.linear_layer.weight.grad)
         linear_loss.backward()
+        print(linear_model.linear_layer.weight.grad)
         linear_optimizer.step()
         linear_optimizer.zero_grad()
 
